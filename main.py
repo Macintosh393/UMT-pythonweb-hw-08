c7a0f5e63231dd044851a4f6fc3fc9e0b32765d4
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2026-05-31 22:25:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from umt-pythonweb-hw-08!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/conf/config.py
@@ -0,0 +1,4 @@
+class Config:
+    DB_URL = "postgresql+asyncpg://postgres:root@localhost:5432/contacts_app"
+
+config = Config
